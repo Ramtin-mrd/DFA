ec049c30b0206e10c71acaa8b5443543ec9dfaac
Ramtin <136721492+Ramtin-mrd@users.noreply.github.com>
2024-05-27 14:42:10 +0330

    Add files via upload

---
+++ xml.py
@@ -0,0 +1,41 @@
+#Ramtin Moradi 40129503
+
+import xml.etree.cElementTree as ET
+
+try:
+    data = ET.parse('xml.xml')
+    root = data.getroot()
+except ET.ParseError as e:
+    print(f"Error parsing XML: {e}")
+    exit(1)
+except FileNotFoundError:
+    print("Error: file was not found.")
+    exit(1)
+
+def xmlReader(root):
+    initialState = root.find('.//initialState').get('name')
+    finalState = {state.get('name') for state in root.findall('.//finalState')}
+    transition = {(trans.get('source'), trans.get('label')): trans.get('destination') for trans in root.findall('.//transition')}
+    return initialState, finalState, transition
+
+initialState, finalState, transition = xmlReader(root)
+
+def checkString(finalState, initialState, transition, string):
+    selectedState = initialState
+    for char in string:
+        if (selectedState, char) in transition:
+            selectedState = transition[(selectedState, char)]
+        else:
+            return False
+    return selectedState in finalState
+
+print("Enter a string like aab, aba, bbbb, ...: \n" + "Enter end to break")
+
+while(True) :
+    String = input( "String = ")
+    if (String == "end") : break
+    if checkString(finalState, initialState, transition, String):
+        print("Accept")
+    else:
+        print("Reject")
+
